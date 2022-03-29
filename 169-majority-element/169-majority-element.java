ce6f4e7fe19a888246e71e8dd841f9af07a97034
akshatpahwa <47842657+akshatpahwa@users.noreply.github.com>
2022-03-29 10:16:40 +0530

    Time: 13 ms (27.80%), Space: 56.7 MB (26.43%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.get(num) > nums.length/2)
+                return num;
+        }
+        return max;
+    }
+}
