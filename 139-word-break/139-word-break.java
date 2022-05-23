3c7d7f3d091e378564c09f77afa1d76c05f96718
akshatpahwa <47842657+akshatpahwa@users.noreply.github.com>
2022-05-23 10:02:27 +0530

    Time: 23 ms (7.13%), Space: 47.3 MB (26.42%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> wordDictSet = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length() + 1];
+        boolean[] dp = new boolean[s.length()+1];
         dp[0] = true;
+        
         for(int i = 1; i <= s.length(); i++){
             for(int j = 0; j < i; j++){
-                if(dp[j] && wordDictSet.contains(s.substring(j, i))){
+                if(dp[j] && wordDictSet.contains(s.substring(j,i))){
                     dp[i] = true;
                     break;
                 }
@@ -14,5 +15,3 @@ class Solution {
         return dp[s.length()];
     }
 }
-//tc- O(n^3)
-//sc- O(n)
