ca0abaf1680e0ec4017c625ebc18427e6ac83282
akshatpahwa <47842657+akshatpahwa@users.noreply.github.com>
2022-05-28 13:16:31 +0530

    Time: 20 ms (50.90%), Space: 72.8 MB (41.98%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,30 +1,27 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        
-        int row = mat.length;
-        int col = mat[0].length;
+        int row = mat.length, col = mat[0].length;
         Queue<int[]> queue = new LinkedList<>();
         
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 if(mat[i][j] == 0)
-                    queue.offer(new int[] {i,j});
+                    queue.offer(new int[]{i,j});
                 else
                     mat[i][j] = Integer.MAX_VALUE;
             }
         }
         
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
+        int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1}};
         while(!queue.isEmpty()){
-            int[] cell = queue.poll();
-            for(int[] d : dirs){
-                int r = cell[0] + d[0];
-                int c = cell[1] + d[1];
-                if(r < 0 || r >= row || c < 0 || c >= col || mat[r][c] <= mat[cell[0]][cell[1]] + 1)
-                    continue;
-                queue.add(new int[] {r, c});
-                mat[r][c] = mat[cell[0]][cell[1]] + 1;
+            int[] point = queue.poll();
+            for(int[] dir : dirs){
+                int x = dir[0] + point[0];
+                int y = dir[1] + point[1];
+                if(x >= 0 && x < row && y >= 0 && y < col && mat[x][y] > mat[point[0]][point[1]] + 1){
+                    queue.add(new int[]{x,y});
+                    mat[x][y] = mat[point[0]][point[1]] + 1;
+                }
             }
         }
         return mat;
