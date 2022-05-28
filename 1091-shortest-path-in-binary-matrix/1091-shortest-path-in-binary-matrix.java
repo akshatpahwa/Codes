517958f47f256514632e0987abd1251958e915d7
akshatpahwa <47842657+akshatpahwa@users.noreply.github.com>
2022-05-28 12:13:37 +0530

    Time: 26 ms (51.79%), Space: 59.8 MB (46.99%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,14 +1,17 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        if(m == 1 && n == 1 && grid[0][0] == 0)
-            return 1;
-        int result = 2;
+        int row = grid.length, col = grid[0].length, result = 2;
         Queue<int[]> queue = new LinkedList<>();
+        
+        if(row == 1 && col == 1 && grid[0][0] == 0)
+            return 1;
+        
         if(grid[0][0] == 0)
             queue.offer(new int[]{0,0});
-        if(queue.size() == 0 || grid[m-1][n-1] != 0 || grid[0][0] != 0)
+        
+        if(queue.size() == 0 || grid[row-1][col-1] != 0)
             return -1; 
+        
         int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{-1,1},{1,-1}};
         while(!queue.isEmpty()){
             int size = queue.size();
@@ -17,8 +20,8 @@ class Solution {
                 for(int[] dir : dirs){
                     int x = point[0] + dir[0];
                     int y = point[1] + dir[1];
-                    if(x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 0){
-                        if(m - 1 == x && n - 1 == y)
+                    if(x >= 0 && x < row && y >= 0 && y < col && grid[x][y] == 0){
+                        if(row - 1 == x && col - 1 == y)
                             return result;
                         grid[x][y] = 1;
                         queue.offer(new int[]{x,y});
