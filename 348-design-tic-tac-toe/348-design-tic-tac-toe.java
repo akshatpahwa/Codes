d6b97845a4f4005f7413856181e76d6fee157159
akshatpahwa <47842657+akshatpahwa@users.noreply.github.com>
2022-06-23 13:05:56 +0530

    Time: 4 ms (85.03%), Space: 48.3 MB (18.10%) - LeetHub

---
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.java
@@ -0,0 +1,30 @@
+class TicTacToe {
+    int[] rows;
+    int[] cols;
+    int diagonal;
+    int antidiagonal;
+    public TicTacToe(int n) {
+        rows = new int[n];
+        cols = new int[n];
+    }
+    
+    public int move(int row, int col, int player) {
+        int current_player = player == 1 ? 1 : -1;
+        rows[row] += current_player;
+        cols[col] += current_player;
+        if(row == col)
+            diagonal += current_player;
+        if(col == cols.length - row - 1)
+            antidiagonal += current_player;
+        int n = rows.length;
+        if(Math.abs(rows[row]) == n || Math.abs(cols[col]) == n || Math.abs(diagonal) == n || Math.abs(antidiagonal) == n)
+            return player;
+        return 0;
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
