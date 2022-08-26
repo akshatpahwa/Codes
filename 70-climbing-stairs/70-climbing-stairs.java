8881afee90efef173a9337970d81a1d52a139727
akshatpahwa <47842657+akshatpahwa@users.noreply.github.com>
2022-08-26 08:56:59 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (27.89%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n == 0 || n == 1)
+        if(n == 0 || n == 1 || n == 2)
             return n;
-        int first = 1, second = 2;
+        int first = 1, second = 1;
         int third = 0;
-        for(int i = 2; i < n ; i++){
-            third = first + second;
-            first = second;
-            second = third;
+        for(int i = 2; i <= n ; i++){
+            third = first + second;//23
+            first = second;//12
+            second = third;//23
         }
         return second;
     }
